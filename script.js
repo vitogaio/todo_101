dd7a60c21ffa166c8fcc5cf076f878ed45a3099f
vg_MBP <vitogaio@vg-MBP-2.local>
2017-02-04 09:03:17 +0100

    jQ: btn disactivated

--- script.js
+++ script.js
@@ -1,52 +1,59 @@
-
 $("document").ready(function () {
 
     // FIND SOMETHING - Find the HTML-Element
-            var $input     = $('input');
-            var $addButton = $('#addButton');
-            var $openItems = $('.openItems');
-            var $doneItems = $('.doneItems');
+    var $input = $('input');
+    var $addButton = $('#addButton');
+    var $openItems = $('.openItems');
+    var $doneItems = $('.doneItems');
+
 
     // CLICK the button
-            $addButton.on('click', function() {
-    // ADD the input value to a variable
-            var $inputValue = $input.val();
-    // DO SOMETHING = Elements to be added
-            var $newListItem = $('<div class="new_content"></div>');
-            var $divIcons    = $('<div class="icons"></div>');
-            var $h2Text      = $('<h2></h2>');
-            var $faClock     = $("<i class='fa fa-clock-o'></i>");
-            var $faTag       = $("<i class='fa fa-tag'></i>");
+    $addButton.on('click', function () {
+        // ADD the input value to a variable
+        var $inputValue = $input.val();
+        if ($input.val() !== "") {
+            // DO SOMETHING = Elements to be added
+            var $newListItem = $('<div class="task"></div>');
+            var $divIcons = $('<div class="icons"></div>');
+            var $h2Text = $('<h2></h2>');
+            var $faClock = $("<i class='fa fa-clock-o'></i>");
+            var $faTag = $("<i class='fa fa-tag'></i>");
 
-    // PUT the input value into the «h2-Element»
+            // PUT the input value into the «h2-Element»
             $h2Text.text($inputValue);
-    // APPEND Text to the «div class ...</div>»
+            // APPEND Text to the «div class ...</div>»
             $newListItem.append($h2Text);
 
-/*
-====================================================
-    // APPEND div to openDIV
-            $newListItem.append($divIcons);
-====================================================
-*/
+            /*
+             ====================================================
+             // APPEND div to openDIV
+             $newListItem.append($divIcons);
+             ====================================================
+             */
 
-    // APPEND the Awesome Icons
+            // APPEND the Awesome Icons
             $newListItem.append($faClock);
             $newListItem.append($faTag);
 
-    // APPEND new item on top(first Item)
+            // APPEND new item on top(first Item)
             $openItems.prepend($newListItem);
-    // Empty the input field on click
-            $input.val(' ');
+            // Empty the input field on click
+            $input.val('');
+        }
 
-        var $listItem = $('.new_content');
-
-     // APPEND work done to «doneItems»
-            $listItem.on('click', function() {
-                $(this).prependTo($doneItems);
-                $(this).css({'background-color': '$color-lightgrey',
-                             'opacity': '0.3'});
-
-        });
     });
+    var $openItems = $('.openItems');
+
+    // APPEND work done to «doneItems»
+    $openItems.on('click', '.task', function () {
+        $(this).prependTo($doneItems);
+        $(this).css({
+            'background-color': '$color-lightgrey',
+            'opacity': '0.3'
+        });
+
+    });
+
+
 });
+
